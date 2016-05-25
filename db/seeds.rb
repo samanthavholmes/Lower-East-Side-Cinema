a66b01392b1a673703bf03f072152231dd008e0c
Samantha Holmes <email": null>
2016-05-25 11:44:31 -0400

    Finish seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,128 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+12.times do
+  User.create!(first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: "password",
+    bio: Faker::Lorem.paragraph,
+    picture: Faker::Avatar.image,
+    role: "judge")
+end
+
+20.times do
+  User.create!(first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: "password",
+    bio: Faker::Lorem.paragraph,
+    picture: Faker::Avatar.image)
+end
+
+genres = ["Comedy", "Drama", "Thriller/Horror", "Romance", "Short", "Documentary"]
+
+i = 0
+
+while i < genres.length
+  Genre.create!(title: genres[i], picture: Faker::Avatar.image)
+  i += 1
+end
+
+10.times do
+  Film.create!(title: Faker::Book.title,
+    director: Faker::Name,
+    description: Faker::Lorem.paragraph,
+    picture: Faker::Avatar.image,
+    genre_id: 1)
+end
+
+10.times do
+  Film.create!(title: Faker::Book.title,
+    director: Faker::Name,
+    description: Faker::Lorem.paragraph,
+    picture: Faker::Avatar.image,
+    genre_id: 2)
+end
+
+10.times do
+  Film.create!(title: Faker::Book.title,
+    director: Faker::Name,
+    description: Faker::Lorem.paragraph,
+    picture: Faker::Avatar.image,
+    genre_id: 3)
+end
+
+10.times do
+  Film.create!(title: Faker::Book.title,
+    director: Faker::Name,
+    description: Faker::Lorem.paragraph,
+    picture: Faker::Avatar.image,
+    genre_id: 4)
+end
+
+10.times do
+  Film.create!(title: Faker::Book.title,
+    director: Faker::Name,
+    description: Faker::Lorem.paragraph,
+    picture: Faker::Avatar.image,
+    genre_id: 5)
+end
+
+10.times do
+  Film.create!(title: Faker::Book.title,
+    director: Faker::Name.name,
+    description: Faker::Lorem.paragraph,
+    picture: Faker::Avatar.image,
+    genre_id: 6)
+end
+
+10.times do
+  Rating.create!(stars: rand(1..5),
+    review: Faker::Lorem.paragraph,
+    user_id: rand(1..20),
+    film_id: rand(1..10))
+end
+
+10.times do
+  Rating.create!(stars: rand(1..5),
+    review: Faker::Lorem.paragraph,
+    user_id: rand(1..20),
+    film_id: rand(10..20))
+end
+
+10.times do
+  Rating.create!(stars: rand(1..5),
+    review: Faker::Lorem.paragraph,
+    user_id: rand(1..20),
+    film_id: rand(20..30))
+end
+
+10.times do
+  Rating.create!(stars: rand(1..5),
+    review: Faker::Lorem.paragraph,
+    user_id: rand(1..20),
+    film_id: rand(30..40))
+end
+
+10.times do
+  Rating.create!(stars: rand(1..5),
+    review: Faker::Lorem.paragraph,
+    user_id: rand(1..20),
+    film_id: rand(40..50))
+end
+
+10.times do
+  Rating.create!(stars: rand(1..5),
+    review: Faker::Lorem.paragraph,
+    user_id: rand(1..20),
+    film_id: rand(50..60))
+end
+
+20.times do
+  Comment.create!(body: Faker::Lorem.paragraph,
+    user_id: rand(1..20),
+    rating_id: (1..60))
+end
+
+
+
+
+
