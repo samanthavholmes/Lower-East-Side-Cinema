48ad391b4b4529827c95f3f6a0dbc5a9109fb739
Samantha Holmes <email": null>
2016-05-25 16:52:13 -0400

    Add links to users for comments and reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   validates_presence_of :first_name, :last_name, :email, :role
   validates_uniqueness_of :email
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
-  validates_length_of :password, within: 6..25, too_long: "password must be less than 25 characters", too_short: "password must be more than 6 characters"
+  validates_length_of :password, within: 6..25, too_long: "must be less than 25 characters", too_short: "must be more than 6 characters"
 
   has_many :ratings
   has_many :comments
