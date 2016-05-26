99198b18cd05764cc495fcdd096537bfbc817c61
sergener <sibel.ergener@gmail.com>
2016-05-26 12:08:40 -0400

    Add some class and ids to get ajax working for both getting the review form on the page and posting the review

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,27 +1,26 @@
 $(document).ready(function() {
 	$('.form-link').on('click', function(e) {
     e.preventDefault();
-		
+		var appendId = e.target.parentElement.id;
 	$.ajax({ //this targets the link, shows form
 	    type: "GET",
 	    url: e.target.href
 	}).done(function(response) {
-	    $(".rating-body").append(response);
+	    $("#"+appendId).append(response);
 	    $('.form-link').hide();
 	  });
 	});
 
 	$('.rating-body').on('submit', ".new_rating", function(e) {
     e.preventDefault();
-		// debugger; 
+    var appendId = $(e.target).parent().find('ul');
 	$.ajax({ //this targets the link, shows form
 	    type: e.target.method,
 	    url: e.target.action,
 	    data: $(e.target).serialize()
 	}).done(function(response) {
-	    $(".rating").append(response);
+	    $(appendId).append(response);
 	    $('#new_rating').remove();
-	    $('.form-link').show();
 	  });
 	});
 });
