e29bed71e8ca4a91988da745f2f55f93966955bd
Leo Kukhar <leokukhar@gmail.com>
2016-05-26 10:38:31 -0400

    Add Working Sort Tabs With Jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,25 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
+
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+$(document).ready(function(){
+
+  $("#star_sort").on("click",function(event){
+    $target = $(event.target)
+    $target.parents().find(".clicked").removeClass("clicked");
+    $target.parent().addClass("clicked");
+    $(".active").hide();
+    $("#sort_by_stars").addClass("active");
+    $("#sort_by_stars").show();
+  });
+
+  $("#recent_sort").on("click",function(event){
+    $target = $(event.target)
+    $target.parents().find(".clicked").removeClass("clicked");
+    $target.parent().addClass("clicked");
+    $(".active").hide();
+    $("#sort_by_recent").addClass("active");
+    $("#sort_by_recent").show();
+  });
+});
