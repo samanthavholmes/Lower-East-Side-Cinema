ea1f6090e2aa87ed49af10f2ee33fb7663f5fbbf
sergener <sibel.ergener@gmail.com>
2016-05-26 12:14:18 -0400

    Fix jquery issues caused by merge conflict

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,31 +28,6 @@ $(document).ready(function() {
 	    $('#new_rating').remove();
 	  });
 	});
-});
-
-  $('.form-link').on('click', function(event) {
-    event.preventDefault();
-    $.ajax({ //this targets the link, shows form
-      type: "GET",
-      url: event.target.href
-    }).done(function(response) {
-      $(".rating-body").append(response);
-      $('.form-link').hide();
-    });
-  });
-
-  $('.rating-body').on('submit', ".new_rating", function(event) {
-    event.preventDefault();
-    $.ajax({ //this targets the link, shows form
-      type: e.target.method,
-      url: e.target.action,
-      data: $(e.target).serialize()
-    }).done(function(response) {
-      $(".rating").append(response);
-      $('#new_rating').remove();
-      $('.form-link').show();
-    });
-  });
 
   $("#star_sort").on("click",function(event){
     $target = $(event.target)
@@ -71,4 +46,4 @@ $(document).ready(function() {
     $("#sort_by_recent").addClass("active");
     $("#sort_by_recent").show();
   });
-};
+});
